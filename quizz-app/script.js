4c2966bd1972306282370ba8d19da2ea4d51ae6e
Kay <kyv.hardeman@gmail.com>
2021-08-26 16:15:14 -0400

    first commit

---
+++ quizz-app/script.js
@@ -0,0 +1,43 @@
+//what we need - data, array of quizzes, question answers and correct answer
+const quizData = [
+  {
+    question: "how old am I?",
+    a: "10",
+    b: "17",
+    c: "26",
+    d: "110",
+    correct: "c",
+  },
+  {
+    question: "What is the most used programming language in 2019?",
+    a: "Java",
+    b: "Javascript",
+    c: "C",
+    d: "Python",
+    correct: "b",
+  },
+  {
+    question: "Who is the president of US?",
+    a: "Beyonce",
+    b: "Kehlani",
+    c: "Biden",
+    d: "Jhene",
+    correct: "c",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Helicopters Terminals Motorboats Lamborghinis",
+    c: "Hyper Markup Logistics",
+    d: "Hyper Markup Language",
+    correct: "a",
+  },
+  {
+    question: "What year was JS launched?",
+    a: "1996",
+    b: "2005",
+    c: "1999",
+    d: "none of the above",
+    correct: "d",
+  },
+];
