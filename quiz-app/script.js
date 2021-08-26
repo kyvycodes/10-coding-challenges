e04c53fb71046c66d4e4c57863d9d3f25d016ec5
Kay <kyv.hardeman@gmail.com>
2021-08-26 18:56:08 -0400

    basic js quiz functionality

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -41,3 +41,37 @@ const quizData = [
     correct: "d",
   },
 ];
+
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+//calls every time button is pressed
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+
+  questionElement.innerHTML = currentQuizData.question;
+
+  a_text.innerHTML = currentQuizData["a"];
+  b_text.innerHTML = currentQuizData["b"];
+  c_text.innerHTML = currentQuizData.c;
+  d_text.innerHTML = currentQuizData.d;
+}
+
+submitBtn.addEventListener("click", () => {
+  currentQuiz++;
+
+  if (currentQuiz < quizData.length) {
+    loadQuiz();
+  } else {
+    //TODO show results
+    alert("you have finished the quiz");
+  }
+});
