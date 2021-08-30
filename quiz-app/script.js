da929a3b95a8ec26f3272b3fbe2e28704a0c9fd8
Kay <kyv.hardeman@gmail.com>
2021-08-29 22:38:00 -0400

    reload the game

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -97,7 +97,7 @@ submitBtn.addEventListener("click", () => {
     if (currentQuiz < quizData.length) {
       loadQuiz();
     } else {
-      quiz.innerHTML = `<h2> Great Job! You answered correctly ${score}/${quizData.length} questions </h2>`;
+      quiz.innerHTML = `<h2> Great Job! You answered correctly ${score}/${quizData.length} questions </h2> <button onclick='location.reload()'>Try again..</button>`;
     }
   }
 });
