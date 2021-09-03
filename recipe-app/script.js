1d487cab171f20414c13907e5a89fd5700ac0147
Kay <kyv.hardeman@gmail.com>
2021-09-03 13:34:35 -0400

    feat: random meal

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,11 +1,23 @@
-function getRandomMeal() {
-  fetch("www.themealdb.com/api/json/v1/1/random.php");
+getRandomMeal();
+
+async function getRandomMeal() {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+
+  const respData = await resp.json();
+  const randomMeal = respData.meals[0];
+  console.log(randomMeal);
 }
 
-function getRandomMealById(id) {
-  fetch("www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+async function getRandomMealById(id) {
+  const meal = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
 }
 
-function getMealsBySearch(factor) {
-  fetch("www.themealdb.com/api/json/v1/1/search.php?s=" + factor);
-}
+// async function getMealsBySearch(factor) {
+//   const meals = await fetch(
+//     "https://www.themealdb.com/api/json/v1/1/search.php?s=" + factor
+//   );
+// }
