a132a13c583b7dd4461df86f0f8ea6209833d821
Kay <kyv.hardeman@gmail.com>
2021-09-05 19:40:27 -0400

    bug: text area eventL

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,6 +1,17 @@
 const editButton = document.querySelector(".edit");
-const deletButton = document.querySelector(".delete");
+const deleteButton = document.querySelector(".delete");
 const notesDiv = document.querySelector(".notes");
 
 const main = notesDiv.querySelector(".main");
 const textArea = notesDiv.querySelector("textarea");
+
+editButton.addEventListener("click", () => {
+  main.classList.toggle("hidden");
+  textArea.classList.toggle("hidden");
+});
+
+textArea.addEventListener("input", (e) => {
+  const { value } = e.target;
+
+  main.innerHtml = marked(value);
+});
