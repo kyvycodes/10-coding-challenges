e8aef0c6a01e58bfced270bb04eb99e3a393f21c
Kay <kyv.hardeman@gmail.com>
2021-09-05 19:09:32 -0400

    connection by querySelectors

---
+++ notes-app/script.js
@@ -0,0 +1,6 @@
+const editButton = document.querySelector(".edit");
+const deletButton = document.querySelector(".delete");
+const notesDiv = document.querySelector(".notes");
+
+const main = notesDiv.querySelector(".main");
+const textArea = notesDiv.querySelector("textarea");
