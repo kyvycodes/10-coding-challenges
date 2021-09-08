30445500e7b93022c5f3e927eeeb032139b73399
Kay <kyv.hardeman@gmail.com>
2021-09-07 22:26:26 -0400

    todo element appended

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,14 +1,17 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const todos = document.getElementById("todos");
 
 form.addEventListener("submit", (e) => {
   //prevents submit
   e.preventDefault();
 
-  const todo = input.value;
-  const todos = [];
+  const todoText = input.value;
 
-  if (todo) {
-    todos.push(todo);
+  if (todoText) {
+    const todoElement = document.createElement("li");
+    todoElement.innerText = todoText;
+
+    todos.appendChild(todoElement);
   }
 });
