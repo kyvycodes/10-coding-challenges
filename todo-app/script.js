308c78900fbb14828cccc89933c30e24d14b9ebf
Kay <kyv.hardeman@gmail.com>
2021-09-07 22:01:08 -0400

    todo event listener

---
+++ todo-app/script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit", (e) => {
+  //prevents submit
+  e.preventDefault();
+
+  const todo = input.value;
+  const todos = [];
+  if (todo) {
+    todos.push(todo);
+  }
+});
