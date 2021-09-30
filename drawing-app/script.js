b3c0d6408f02879b0edf4539c2bb2c8e2fa28ad8
Kay <kyv.hardeman@gmail.com>
2021-09-30 19:46:18 -0400

    feat: separate circles at mouse move

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -5,14 +5,28 @@ const ctx = canvas.getContext('2d')
 
 let size = 20;
 
-canvas.addEventListener('mousedown', () => {
 
+canvas.addEventListener('mousemove', (event) => {
+
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  drawCircle(x,y)
 })
+
+canvas.addEventListener()
 
 function drawCircle(x,y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2)
-  ctx.stroke()
+  ctx.fill()
 }
 
 drawCircle(50, 50)
+
+
+// function draw() {
+//   drawCircle(50, 50)
+
+//   requestAnimationFrame()
+// }
