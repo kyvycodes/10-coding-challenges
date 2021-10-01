d36fe3f7bd960f14b7d97ab3a02c7a01ab497306
Kay <kyv.hardeman@gmail.com>
2021-09-30 20:03:55 -0400

    feat: size adjustment

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -1,7 +1,10 @@
-const canvas = document.getElementById('canvas')
 // https://www.w3schools.com/html/html5_canvas.asp
 
+const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
+
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
 
 let size = 20;
 let isCurrentlyPressed = false;
@@ -25,7 +28,25 @@ canvas.addEventListener('mousedown', (event) => {
   isCurrentlyPressed = true
 })
 
-canvas.addEventListener()
+
+
+increaseBtn.addEventListener('click', () => {
+  size += 5
+
+  if(size > 50) {
+    size = 50;
+  }
+})
+
+decreaseBtn.addEventListener('click', () => {
+  size -= 5;
+
+  if(size < 5){
+    size = 5;
+  }
+
+})
+
 
 function drawCircle(x,y) {
   ctx.beginPath();
